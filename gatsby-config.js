aaf81ca53280fa69acdbca1616e468e40aa89357
alarivan <alarivan@pm.me>
2019-09-04 13:01:14 -0700

    Replace GA gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,9 +46,12 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingIds: [process.env.GA_TRACKING_ID],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
       },
     },
 
